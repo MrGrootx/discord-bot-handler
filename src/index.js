62e129108405f3c238637d186c6229fb8a412021
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-04-20 14:21:43 +0530

    feat: error-handler

--- src/index.js
+++ src/index.js
@@ -1,21 +1,104 @@
-const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, MessageManager, Embed, Collection } = require(`discord.js`);
-const fs = require('fs');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] }); 
+const {
+  AttachmentBuilder,
+  MessageType,
+  Client,
+  Partials,
+  GatewayIntentBits,
+  PermissionFlagsBits,
+  EmbedBuilder,
+  PermissionsBitField,
+  Permissions,
+  MessageManager,
+  Embed,
+  Collection,
+  Events,
+  AuditLogEvent,
+  MessageCollector,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ChannelType,
+  Integration,
+  ComponentType,
+  GuildPresences,
+  ActivityType,
+  ChatInputCommandInteraction,
+} = require(`discord.js`);
+const fs = require("fs");
+const process = require("node:process");
+const client = new Client({
+  intents: [
+    Object.keys(GatewayIntentBits),
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.GuildPresences,
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.User,
+    Partials.GuildMember,
+    Partials.Reaction,
+  ],
+  presence: {
+    status: "idle",
+  },
+});
+
+const fs = require("fs");
+const process = require("node:process");
+// const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.commands = new Collection();
 
-require('dotenv').config();
+require("dotenv").config();
 
-const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
-const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
+const functions = fs
+  .readdirSync("./src/functions")
+  .filter((file) => file.endsWith(".js"));
+const eventFiles = fs
+  .readdirSync("./src/events")
+  .filter((file) => file.endsWith(".js"));
 const commandFolders = fs.readdirSync("./src/commands");
 
 (async () => {
-    for (file of functions) {
-        require(`./functions/${file}`)(client);
-    }
-    client.handleEvents(eventFiles, "./src/events");
-    client.handleCommands(commandFolders, "./src/commands");
-    client.login(process.env.TOKEN)
+  for (file of functions) {
+    require(`./functions/${file}`)(client);
+  }
+  client.handleEvents(eventFiles, "./src/events");
+  client.handleCommands(commandFolders, "./src/commands");
+  client.login(process.env.TOKEN);
 })();
 
+process.on("unhandledRejection", async (reason, promise) => {
+  console.log("unhandled rejection at:", promise, "reason:", reason);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception:", err);
+});
+
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+  console.log("Uncaught Exception Monitor:", err, origin);
+});
+
+process.on("rejectionHandled", (err) => {
+  console.log("rejected handled:", err);
+});
+
+process.on("warning", (warning) => {
+  console.log("Warning:", warning);
+});
